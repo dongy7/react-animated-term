80d67ba4686faedb03f74a5815bcf41184a35f00
Dong Yeop Lee <dongy7@gmail.com>
2018-05-11 15:35:17 -0400

    add support for framed output

--- src/contentHandler.js
+++ src/contentHandler.js
@@ -1,6 +1,7 @@
 function* terminalContent(lines) {
   let lineIndex = 0
   let pos = 0
+  let frameIndex = 0
   const currLines = []
   if (lines.length === 0) {
     return []
@@ -9,13 +10,33 @@ function* terminalContent(lines) {
   while (true) {
     if (lineIndex < lines.length) {
       if (!lines[lineIndex].cmd) {
-        currLines.push({
-          text: lines[lineIndex].text,
-          cmd: false,
-          current: false
-        })
-        pos = 0
-        lineIndex++
+        const frames = lines[lineIndex].frames
+        if (!frames) {
+          currLines.push({
+            text: lines[lineIndex].text,
+            cmd: false,
+            current: false
+          })
+          yield currLines
+          pos = 0
+          lineIndex++
+        } else if (frameIndex < frames.length) {
+          if (frameIndex === 0) {
+            currLines.push({
+              text: frames[0],
+              cmd: false,
+              current: true,
+            })
+          }
+          currLines[lineIndex].text = frames[frameIndex]
+          frameIndex++
+          yield currLines
+        } else {
+          pos = 0
+          frameIndex = 0
+          currLines[lineIndex].current = false
+          lineIndex++
+        }
       } else if (pos == 0) {
         currLines.push({
           text: '',
