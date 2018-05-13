ef11eaf46805ab4cd8b2b29cf0f3a000e21f2c3d
Dong Yeop Lee <dongy7@gmail.com>
2018-05-13 15:01:53 -0400

    add webpack config for building example

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  context: path.resolve(__dirname, 'examples/src'),
+  entry: {
+    app: './app.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'examples/dist'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'examples/src'),
+    port: 8000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: { presets: ['env'] }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'react-animated-term': path.resolve(__dirname, 'src/index')
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: false,
+      template: path.resolve(__dirname, 'examples/src/index.html')
+    })
+  ]
+}
